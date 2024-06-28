200e2f9925bbdf1aecbbf260fbfc5c5c31bc1599
EjayJab <107586978+EjayJab@users.noreply.github.com>
2024-06-28 13:58:16 +0000

    Updated app.js with enhancements

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
-const express=require('express')
-var path = require('path');
-global.appRoot = path.resolve(__dirname);
-const routes = require('./routes/getRoutes');
-const events=require('./routes/event');
-const restroutes=require('./REST/restRoutes');
-const photoroutes=require('./REST/photo');
-const jobpoolroutes=require('./REST/jobpool');
-const mongo=require('./mongoConfig')
-const authuser=require('./REST/authuser')
-const teams=require('./REST/team')
-const tasks=require('./REST/task')
+const express = require('express');
+const path = require('path');
 const fileUpload = require('express-fileupload');
-const app=express()
+const cookieSession = require('cookie-session');
+const mongo = require('./mongoConfig');
+const routes = require('./routes/getRoutes');
+const events = require('./routes/event');
+const restroutes = require('./REST/restRoutes');
+const photoroutes = require('./REST/photo');
+const jobpoolroutes = require('./REST/jobpool');
+const authuser = require('./REST/authuser');
+const teams = require('./REST/team');
+const tasks = require('./REST/task');
+const profileRoutes = require('./REST/profile');
+
+global.appRoot = path.resolve(__dirname);
+
+const app = express();
+
+// Middleware
 app.use(fileUpload());
-mongo();
-const profileRoutes=require("./REST/profile")
-var cookieSession = require('cookie-session')
 app.use(cookieSession({
   name: 'session',
   keys: ["sathish"],
   maxAge: 48 * 60 * 60 * 1000 // 48 hours
-}))
-app.use(express.json())
-app.use(express.static('./public'))
-app.use(routes)
-app.use(restroutes)
-app.use(events)
-app.use(authuser)
-app.use(jobpoolroutes)
-app.use(profileRoutes)
-app.use(photoroutes)
-app.use(teams)
-app.use(tasks)
-app.listen(8000, console.log("app is listening"))
+}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static('./public'));
+
+// Database connection
+mongo();
+
+// Routes
+app.use(routes);
+app.use(restroutes);
+app.use(events);
+app.use(authuser);
+app.use(jobpoolroutes);
+app.use(profileRoutes);
+app.use(photoroutes);
+app.use(teams);
+app.use(tasks);
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
